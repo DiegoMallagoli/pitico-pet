3756bfe975ed24284ebf8d8aae2e331ab8bee0ee
DiegoMallagoli <reisdiego121@gmail.com>
2024-10-20 17:36:40 -0300

    teste

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    function exibirMensagem() {
+        alert("Estamos te encarregando para a outra página!! Confirme em 'OK' para prosseguir");
+    }
+    document.getElementById('Menu1').onclick = exibirMensagem;
+    });
+    function exibirMensagem() {
+        alert("Estamos te encarregando para a outra página!! Confirme em 'OK' para prosseguir");
+    }
+    document.getElementById('Menu2').onclick = exibirMensagem;
+    function exibirMensagem() {
+        alert("Estamos te encarregando para a outra página!! Confirme em 'OK' para prosseguir");
+    }
+    document.getElementById('Menu3').onclick = exibirMensagem;
